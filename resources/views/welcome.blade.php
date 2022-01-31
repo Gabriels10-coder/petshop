e6cde768b6f7256d6efd5f0a4f6b563b2641f36f
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2022-01-30 21:36:30 -0300

    adc bootstrasp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Petshop</title>
 </head>
 
 <body>
-    <h1>Bem vindo a home</h1>
+    <form method="POST">
+        <div class="flex justify-content-center">
+            <div>
+                <input class="form-control form-control-sm " type="email">
+            </div>
+
+        </div>
+    </form>
 </body>
 
 </html>
