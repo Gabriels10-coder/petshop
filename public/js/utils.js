579b9df8ae372a17e174b8c1ca22d62ad5847ace
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2022-02-16 22:16:13 -0300

    testando validação

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,1 +1,19 @@
+function validacaoPets() {
+    //objeto  para pegar valores dos inputs
 
+    let inputs = {
+        nomeanimal: document.getElementById("nomeanimal").value,
+        raca: document.getElementById("raca").value,
+        peso: document.getElementById("peso").value,
+        idade: document.getElementById("idade").value,
+        dono: document.getElementById("dono").value,
+    };
+    let tamanhonome = inputs.nomeanimal.toString().length;
+    console.log(tamanhonome);
+
+     if (tamanhonome < 10) {
+        document.getElementById("erro1").innerHTML = "teste";
+    }else{
+        document.getElementById("erro1").innerHTML = "nada";
+    }
+}
