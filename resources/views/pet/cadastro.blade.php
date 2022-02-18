a5794cda4d53300032157dc7cc795bf0a79d8f0f
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2022-02-17 21:46:57 -0300

    aprendendo a validar com js puro

--- resources/views/pet/cadastro.blade.php
+++ resources/views/pet/cadastro.blade.php
@@ -3,24 +3,30 @@
 @section('conteudo')
     <form method="POST" action="{{ route('registro.pet') }}">
         @csrf
-        <section  class="container distnavbar ">
+        <section class="container distnavbar ">
             <div class="d-flex flex-column align-items-center ">
                 <div class="col-6 ">
                     <label class="form-label">Nome do animal:</label>
-                    <input class="form-control" type="text" name="nome" onkeyup="validacaoPets()" placeholder="Insira o nome  do seu animal" id="nomeanimal" />
-                    <p id="erro1"></p>
+                    <input class="form-control" type="text" name="nome" onkeyup="validacaoPets()"
+                        placeholder="Insira o nome  do seu animal" id="nomeanimal" re />
+                    <p id="validanome"></p>
                 </div>
                 <div class="col-6">
                     <label class="form-label">raça:</label>
-                    <input class="form-control" type="text" name="raca" placeholder="Insira a raça do seu animal" id="raca" />
+                    <input class="form-control" type="text" name="raca" placeholder="Insira a raça do seu animal"
+                        id="raca" onkeyup="validacaoPets()"/>
+                    <p id="validaraca"></p>
                 </div>
                 <div class="col-6">
                     <label class="form-label">Peso:</label>
-                    <input class="form-control" type="number"  name="peso" placeholder="Insira o peso atual do animal" id="peso" />
+                    <input class="form-control" type="text"onkeyup="validacaoPets()" name="peso" placeholder="Insira o peso atual do animal"
+                        id="peso" />
+                        <p id="validapeso"></p>
                 </div>
                 <div class="col-6">
                     <label class="form-label">Idade:</label>
-                    <input class="form-control" type="number" name="idade"  placeholder="Insira a idade do seu animal" id="idade" />
+                    <input class="form-control" type="number" name="idade" placeholder="Insira a idade do seu animal"
+                        id="idade" />
 
                 </div>
                 <div class="col-6">
@@ -28,14 +34,11 @@
                     <input class="form-control" type="text" name="dono" placeholder="Quem é o dono" id="dono" />
                 </div>
                 <div class="col-6 container-btn">
-                    <button class="btn btn-outline-primary" id="sucesso" type="submit">Enviar</button>
+                    <button id="btn-submit" class="btn btn-outline-primary" id="sucesso" type="submit">Enviar</button>
                 </div>
 
             </div>
         </section>
 
     </form>
-    @endsection
-
-
-
+@endsection
