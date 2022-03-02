3ce4e86ba95d7590f73c554837ddeb01b9a5009c
Gabriel <gsm2015@outlook.com.br>
2022-03-02 20:57:34 -0300

    layout edit

--- resources/views/pet/editar.blade.php
+++ resources/views/pet/editar.blade.php
@@ -5,37 +5,42 @@
 <link rel="stylesheet" href="{{asset('css/editarpet.css')}}"/>
 @endsection
 @section('conteudo')
-    <main style="margin-top: 30px">
-        <section class="container"  >
+    <main>
+        <section class="container"    >
             <form method="POST">
                 @csrf
                 @method("PUT")
-                <div style="display: flex; justify-content:center;">
-                <section id="test-container"  >
-
-                    <div class="form-group nomeanimal">
+                <section class="container--edit">
+                    <div class="container--titulo">
+                        <h1>Edite seu registro</h1>
+                    </div>
+                    <div class="container--nome">
                         <label class="form-label" for="nome">Nome do animal</label>
                         <input class="form-control"  type="text" name="nome" id="nome" />
                     </div>
-                    <div class="form-group raça ">
+                    <div class="container--raca">
                         <label class="form-label"  for="raca">Raça</label>
                         <input class="form-control" type="text" name="raça" id="raca" />
                     </div>
-                    <div class="form-group peso">
+                    <div class="container--peso">
                         <label class="form-label" for="peso">Peso</label>
                         <input class="form-control"type="text" name="peso" id="peso" />
                     </div>
-                    <div class="form-group idade">
+                    <div class="container--idade">
                         <label class="form-label" for="idade">Idade</label>
                         <input class="form-control" type="number" name="idade" id="idade" />
                     </div>
-                    <div class="form-group espec">
-                        <label class="form-label" for="especie">especie</label>
+                    <div class="container--espec">
+                        <label class="form-label" for="especie">Espécie</label>
                         <input class="form-control" type="text" name="especie" id="especie" />
+                    </div>
+                    <div  class="container--buttons">
+                        <button class="btn btn-outline-primary btn-person">Editar</button>
+                        <button class="btn btn-outline-primary btn-person">Cancelar</button>
                     </div>
                 </section>
 
-</div>
+
 
             </form>
 
